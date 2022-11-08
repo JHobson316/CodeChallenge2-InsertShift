b5b02c3a16fe0a1c1e3eee059178ece9fe93a1fe
Jordan Hobson <snoopyjchris@gmail.com>
2022-11-07 23:35:29 -0600

    Pushing Work

---
+++ CodeChallenge2-InsertShift/InsertShift.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenge2_InsertShift
+{
+    class InsertShift
+    {
+        public static int[] ShiftIn(int[] array, int num)
+        {
+            int n = array.Length+1;
+            int[] Shifted = new int[n];
+            //int counter = 0;
+            int pos = 5;
+            for (int i = 0; i<n; i++)
+            {
+                if (i < pos)
+                {
+                    Shifted[i] = array[i];
+                }
+                else if (i == pos)
+                {
+                    Shifted[i] = num;
+                }
+                else
+                {
+                    Shifted[i] = array[i - 1];
+                }
+            }
+            foreach (int number in Shifted)
+            {
+                Console.WriteLine(number);
+            }
+            return Shifted;
+        }
+    }
+}
---
+++ CodeChallenge2-InsertShift/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CodeChallenge2_InsertShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            int[] ray = { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            ShiftIn(ray, 100);
+        }
+        static int[] ShiftIn(int[] array, int num)
+        {
+            //int i;
+            int n = array.Length + 1;
+            int[] Shifted = new int[n];
+            int pos = 5;
+            for (int i = 0; i < n; i++)
+            {
+                if (i < pos)
+                {
+                    Shifted[i] = array[i];
+                }
+                else if (i == pos-1)
+                {
+                    Shifted[i] = num;
+                }
+                else
+                {
+                    Shifted[i] = array[i - 1];
+                }
+            }
+            foreach (int number in Shifted)
+            {
+                Console.WriteLine(number);
+            }
+            return Shifted;
+        }
+    }
+}
